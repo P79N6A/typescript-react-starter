ce3c1868afa5417bc06b9f9db45ceab8e828575c
huangchaolin <huangchaolin@xylink.com>
2018-09-05 16:44:41 +0800

    feat: add alias support for typescript & webpack.

--- config/public.config.js
+++ config/public.config.js
@@ -1,4 +1,7 @@
+const paths = require('./paths');
+const path = require("path")
 
-
-
-
+exports.alias = {
+  'assets': path.resolve(paths.appSrc, 'assets'),
+  'components': path.resolve(paths.appSrc, 'components')
+}
--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
-import * as style from './App.scss';
-import logo from './logo.svg';
+import * as style from './index.scss';
+import logo from 'assets/logo.svg';
 
 class App extends React.Component {
   public render() {
